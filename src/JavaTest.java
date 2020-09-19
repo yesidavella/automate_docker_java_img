fdfba605e3a6c8c3c391a7cf577f491134951656
Yesid-ubuntu <yeavella@gmail.com>
2020-09-19 22:43:22 +0100

    Testing build img automation from githubgit add .

--- src/JavaTest.java
+++ src/JavaTest.java
@@ -2,7 +2,7 @@
 public class JavaTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello-hola, this is the java version from code: "+System.getProperty("java.version"));
+		System.out.println("Hello-hola a todos parceros!!, this is the java version from code: "+System.getProperty("java.version"));
 	}
 
 }
